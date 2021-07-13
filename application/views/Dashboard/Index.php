2e8205bec0ac41c5478b6e8c6f6212412d338b3c
ilfahrifany <ilfahrifany02@gmail.com>
2021-07-13 19:26:45 +0700

    coba

---
+++ application/views/Dashboard/Index.php
@@ -0,0 +1,102 @@
+<!-- Default box -->
+<div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box bg-aqua">
+            <span class="info-box-icon"><i class="fas fa-users"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Customer</span>
+              <span class="info-box-number">1.123</span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 100%"></div>
+              </div>
+                  <span class="progress-description">
+                    Total Customer
+                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box bg-green">
+            <span class="info-box-icon"><i class="fa fa-cubes"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Stok</span>
+              <span class="info-box-number">145.000</span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 100%"></div>
+              </div>
+                  <span class="progress-description">
+                    Total Stok
+                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box bg-yellow">
+            <span class="info-box-icon"><i class="fa fa-shopping-cart"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Transaksi</span>
+              <span class="info-box-number">10.000</span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 100%"></div>
+              </div>
+                  <span class="progress-description">
+                    Transaksi Bulan Ini
+                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box bg-red">
+            <span class="info-box-icon"><i class="fa fa-money-bill-alt"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Pendapatan</span>
+              <span class="info-box-number">Rp. 120.000.000,-</span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 100%"></div>
+              </div>
+                  <span class="progress-description">
+                    Pemasukan Bulan ini
+                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+      </div>
+     <!-- Default box -->
+<div class="col-md-12">
+  <div class="box">
+    <div class="box-header">
+      
+    </div>
+    <div class="box-body">
+
+    <center>
+      <h1> <b> Selamat Datang di Aplikasi Pengelolaan Penjualan
+        <br>TOKO FADJAR COY</br></b></h1>
+<center><img src="<?= base_url('/assets/uploads/image/fcy.jpg'); ?>" class="img img-responsive" width="50%"></center>
+    </center>
+      <!-- /.col -->
+    </div>
+
+  </div>
+
+</div>
